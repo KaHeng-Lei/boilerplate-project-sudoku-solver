363bc118a5a372db84f38117f3317ec1d919b421
otislei <otislei2020@gmail.com>
2023-02-07 22:18:15 +0800

    feat: add route for check and solve

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,81 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
+    if (!puzzle || !coordinate || !value) {
+      res.json({ error: "Required field(s) missing" });
+      return;
+    }
+    const row = coordinate.split("")[0];
+    const column = coordinate.split("")[1];
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    if (
+      coordinate.length !== 2 ||
+      !/[a-i]/i.test(row) ||
+      !/[1-9]/i.test(column)
+    ) {
+      console.log("invalid coordinate :>> ");
+      res.json({ error: "Invalid coordinate" });
+      return;
+    }
+    console.log(value);
+    if (!/[1-9]$/i.test(value)) {
+      console.log("Invalid value: " + value);
+      res.json({ error: "Invalid value" });
+      return;
+    }
+    if (puzzle.length != 81) {
+      res.json({ error: "Expected puzzle to be 81 characters long" });
+      return;
+    }
+    if (/[^0-9.]/g.test(puzzle)) {
+      res.json({ error: "Invalid characters in puzzle" });
+      return;
+    }
+    let validCol = solver.checkColPlacement(puzzle, row, column, value);
+    let validRow = solver.checkRowPlacement(puzzle, row, column, value);
+    let validReg = solver.checkRegionPlacement(puzzle, row, column, value);
+    let conflicts = [];
+    if (validCol && validRow && validReg) {
+      res.json({ valid: true });
+    } else {
+      if (!validRow) {
+        conflicts.push("row");
+      }
+      if (!validCol) {
+        conflicts.push("column");
+      }
+      if (!validReg) {
+        conflicts.push("region");
+      }
+      res.json({ valid: false, conflict: conflicts });
+    }
+  });
 
-    });
+  app.route("/api/solve").post((req, res) => {
+    const { puzzle } = req.body;
+    if (!puzzle) {
+      res.json({ error: "Required field missing" });
+      return;
+    }
+    if (puzzle.length != 81) {
+      res.json({ error: "Expected puzzle to be 81 characters long" });
+      return;
+    }
+    if (/[^0-9.]/g.test(puzzle)) {
+      res.json({ error: "Invalid characters in puzzle" });
+      return;
+    }
+    let solverString = solver.solve(puzzle);
+    if (!solverString) {
+      res.json({ error: "Puzzle cannot be solved" });
+    } else {
+      res.json({ solution: solverString });
+    }
+  });
 };
